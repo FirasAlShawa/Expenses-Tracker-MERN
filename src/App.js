3cb5f44c09ab65e18fab40b605fd2cd237066ece
Firas shawa <firas.shawa22@gmail.com>
2019-06-23 20:47:21 +0300

    App.js & Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React,{ Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Components/Home";
+import GitTracker from "./Components/GitTracker";
+import oneMonth from "./Components/OneMonthTrack";
+import {BrowserRouter as Rounter , Route , Link} from "react-router-dom";
+
 
 function App() {
+  const MainColor ={
+    backgroundColor : "#F2958D"
+  };
+  return (
+    <Rounter>
+        <div className="container" style={MainColor} >
 
-  return <h1>Hi React</h1>
+          <Route path="/" exact strcict component={Home} />
+          <Route path="/GitTracker" exact strcict component={GitTracker}/>
+          <Route path="/GitTracker/TrackType/:Category/" exact strcict component={oneMonth}/>
+          
+          </div>
+        
+      </Rounter>
+    );
 }
 
 export default App;
